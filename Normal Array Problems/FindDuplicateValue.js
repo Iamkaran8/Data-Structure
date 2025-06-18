5a7abc4d0d0f2a0c1a9aaa35d96d5881d03a2ce0
karansenthil8 <karanwebdev8@gmail.com>
2025-06-18 19:11:49 +0530

    Find Duplicate value Of The Array Problem

--- Normal Array Problems/FindDuplicateValue.js
+++ Normal Array Problems/FindDuplicateValue.js
@@ -1,1 +1,42 @@
-console.log("FindDuplicateValue.js")
+const people = [
+    {
+        name: "Alice",
+        age: 21
+    },
+    {
+        name: "Bob",
+        age: 22
+    },
+    {
+        name: "Charlie",
+        age: 23
+    },
+    {
+        name: "Alice",
+        age: 22
+    },
+    {
+        name: "David",
+        age: 21
+    },
+    {
+        name: "Bob",
+        age: 21
+    }
+];
+
+
+const FindDuplicate = (arr) => {
+    const unique = new Set();
+
+    for (val of arr) {
+        if(unique.has(val.age)){
+            console.log("alread Exist :", val)
+        }else{
+            unique.add(val.age)
+        }
+    }
+}
+
+FindDuplicate(people)
+
