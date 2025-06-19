41201d61b5644149df8259be829356f7e656d206
karansenthil8 <karanwebdev8@gmail.com>
2025-06-19 23:18:52 +0530

    First Unique Character Problem Sloved

--- FrequencyPattern/firstUniqueCharacter.js
+++ FrequencyPattern/firstUniqueCharacter.js
@@ -3,7 +3,7 @@
 // Given a string s, find the index of the first non-repeating character.
 // If it doesn't exist, return -1
 
-const str = "loveleetcode";
+const str = "loveloe";
 
 const firstUniqueChar = (str) => {
     const unique = {}
@@ -11,12 +11,13 @@ const firstUniqueChar = (str) => {
     for (let i = 0; i < str.length; i++) {
         unique[str[i]] = (unique[str[i]] || 0) + 1
     }
-    console.log(unique)
-    for (let key in unique) {
-        if (unique[key] === 1) {
-            return key
+
+    for (let i of str) {
+        if (unique[i] === 1) {
+            return i
         }
     }
+    return "No Unique Values"
 }
 
 console.log(firstUniqueChar(str))
