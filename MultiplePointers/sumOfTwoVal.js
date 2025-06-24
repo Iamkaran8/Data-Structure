71172e38f634aba532c42d0d45a7dfbcb4fa7c0d
karansenthil8 <karanwebdev8@gmail.com>
2025-06-24 20:19:52 +0530

    Sloved

--- MultiplePointers/sumOfTwoVal.js
+++ MultiplePointers/sumOfTwoVal.js
@@ -9,7 +9,37 @@ const sumOfTwoVal = (arr) => {
             }
         }
     }
+
+    console.log("---------------Traditional Way Is Done---------------")
 }
 
 
 console.log(sumOfTwoVal([1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8]))
+
+
+
+
+// This is Effecient Way
+
+const Effecient = (arr,target) => {
+    console.log("---------------Efficient Way---------------")
+    let left = 0;
+    let right = arr.length -1;
+    while(left < right){
+        let sum = arr[left] + arr[right];
+        if(sum === target) {
+            return arr[left],arr[right]
+
+        };
+        if(sum <target) left++;
+        else right++
+    }
+    console.log("No Pair FOund")
+}
+
+
+console.log(Effecient([1, 2, 3, 4, 5, 6, 7, 8,],10))
+
+
+
+
