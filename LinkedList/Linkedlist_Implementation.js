353154b5f504620c6882783f2613e81e5395322d
karansenthil8 <karanwebdev8@gmail.com>
2025-07-28 09:54:41 +0530

    linkedlist

--- LinkedList/Linkedlist_Implementation.js
+++ LinkedList/Linkedlist_Implementation.js
@@ -6,42 +6,32 @@
 // }
 
 
-// class LinkedList {
 
+// class LinkedList {
 //     constructor() {
 //         this.head = null;
 //     }
-
-
 //     addFirst(data) {
 //         const newNode = new Node(data);
 //         newNode.next = this.head;
 //         this.head = newNode
 //     }
 
-//     printList() {
+//     printVal() {
 //         let current = this.head;
 //         while (current !== null) {
-//             console.log(current.data,"-->");
-
-//             current = current.next;
+//             console.log(current.data)
+//             current = current.next
 //         }
 //     }
 // }
-
-// const list = new LinkedList;
-
-// list.addFirst(88);
-// list.addFirst(99);
-
-// list.addFirst(111);
-
-
-
-
-// list.printList()
-
-
+// const list = new LinkedList()
+// list.addFirst(11)
+// list.addFirst(12)
+// list.addFirst(13)
+// list.addFirst(14)
+// list.addFirst(15)
+// list.printVal()
 
 
 
@@ -60,10 +50,42 @@ class LinkedList {
 
     addFirst(data) {
         const newNode = new Node(data);
+        newNode.next = this.head;
         this.head = newNode;
-        this.next =
+    }
+
+    addLast(data){
+        const newNode = new Node(data);
+        let current = this.head
+        if(!current){
+            current = newNode;
+            return;
+        }
+
+        while(current.next){
+            current = current.next
+        }   
+
+        current.next = newNode;
     }
 
 
+    printVal() {
+        let current = this.head;
+        while (current !== null) {
+            console.log(current.data)
+            current = current.next
+        }
+    }
 }
 
+
+const arr = new LinkedList();
+arr.addFirst(88)
+arr.addFirst(89)
+arr.addFirst(90)
+arr.addLast(0)
+arr.addFirst(91)
+
+
+arr.printVal()
